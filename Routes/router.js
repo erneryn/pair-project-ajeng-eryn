071e0ffc3859008f821c432de489eaed4805b3dd
ajengrf <ajengrf.12@gmail.com>
2020-01-30 16:12:37 +0700

    controller

--- Routes/router.js
+++ Routes/router.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const ControllerPlaylist = require('../controller/controllerPlaylist')
 
 router.get('/', function (req, res) {
   res.send('home page')
 })
 
-router.get('/about', function (req, res) {
-  res.send('About')
-})
+router.get('/playlist', ControllerPlaylist.showPlaylist)
+
+router.get('/playlist/edit/:id', ControllerPlaylist.editPlaylist)
+router.get('/playlist/edit/delete/:id', ControllerPlaylist.deleteSong)
+
+router.get('/playlist/delete/:id', ControllerPlaylist.deletePlaylist)
+
+router.get('/:id/myPlaylist', ControllerPlaylist.showMyPlaylist)
 
 module.exports = router
--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
     gender: DataTypes.STRING,
     email: DataTypes.STRING,
     age: DataTypes.INTEGER
-  })
+  }, { sequelize })
 
   User.associate = function (models) {
     // associations can be defined here
