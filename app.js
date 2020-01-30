6529488e9b22c2ac6c8166294a6782a12ee0e697
eryern <erneryn@gmail.com>
2020-01-31 06:27:59 +0700

    change port

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const nodemailer = require('nodemailer')
 
 const homeRouter = require('./Routes/homeRoute')
-const userRouter = require('./Routes/userRoute')
 const playlistRouter = require('./Routes/playlistRoute')
 const session = require('express-session')
 
@@ -20,7 +18,6 @@ app.set('view engine', 'ejs')
 
 app.use('/', homeRouter)
 
-app.use('/user', userRouter)
 app.use('/playlist', playlistRouter)
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
