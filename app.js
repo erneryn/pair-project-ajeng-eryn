58d884f104933442c4eb9f276e8de0b0f794312b
eryern <erneryn@gmail.com>
2020-01-31 07:32:02 +0700

    add cool

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 const homeRouter = require('./Routes/homeRoute')
 const playlistRouter = require('./Routes/playlistRoute')
 const session = require('express-session')
 
 app.use(express.static('public'))
+
+app.get('/cool', (req, res) => res.send(cool()))
+
+
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true
 }))
+
 
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
@@ -20,4 +26,4 @@ app.use('/', homeRouter)
 
 app.use('/playlist', playlistRouter)
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}!`))
